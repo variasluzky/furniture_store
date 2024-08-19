58b4f8c2e0c5ba062a9e8b8c290ade314332796e
Varya Slutsky <varia.sluzky@wdc.com>
2024-08-19 16:41:35 +0300

    updated files for divided front and back

--- furniture_store/src/main/java/com/ltp/furniture_store/web/RegistrationController.java
+++ furniture_store/src/main/java/com/ltp/furniture_store/web/RegistrationController.java
@@ -2,6 +2,7 @@ package com.ltp.furniture_store.web;
 import com.ltp.furniture_store.entity.RegisteredCustomer;
 import com.ltp.furniture_store.service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,9 +14,21 @@ public class RegistrationController {
     private RegistrationService registrationService;
 
     @PostMapping("/register")
-    public ResponseEntity<RegisteredCustomer> registerUser(@RequestBody RegisteredCustomer registeredCustomer) {
-        RegisteredCustomer savedCustomer = registrationService.registerUser(registeredCustomer);
-        return ResponseEntity.ok(savedCustomer);
+    public ResponseEntity<?> registerUser(@RequestBody RegisteredCustomer registeredCustomer) {
+        System.out.println("Received registration request with data: " + registeredCustomer);
+        if (registeredCustomer.getPassword() == null) {
+            System.out.println("Password is null for incoming registration request.");
+            return ResponseEntity.badRequest().body("Password cannot be null");
+        }
+        try {
+            RegisteredCustomer savedCustomer = registrationService.registerUser(registeredCustomer);
+            return ResponseEntity.ok(savedCustomer);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing registration: " + e.getMessage());
+        }
     }
+
+
 }
 
