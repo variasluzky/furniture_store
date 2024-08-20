4e4f0b618888b0213981890639a58099043d72c3
Varya Slutsky <varia.sluzky@wdc.com>
2024-08-20 14:43:03 +0300

    reverse

--- furniture_store/src/main/java/com/ltp/furniture_store/service/RegisteredCustomerService.java
+++ furniture_store/src/main/java/com/ltp/furniture_store/service/RegisteredCustomerService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 
 @Service
-    public class RegistrationService {
+    public class RegisteredCustomerService {
 
         @Autowired
         private RegisteredCustomerRepository registeredCustomerRepository;
